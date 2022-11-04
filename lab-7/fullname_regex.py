1f093f0d1ae307db58a3140b75a97ad258bf6f42
thevalstar <94736197+thevalstar@users.noreply.github.com>
2022-11-04 15:58:31 -0500

    Add name regex

--- lab-7/fullname_regex.py
+++ lab-7/fullname_regex.py
@@ -11,10 +11,19 @@ import re  # compile(), search()
 
 
 def main():
+    # regex to use
+    name_regex = re.compile(r'''
+    ^[a-zA-Z] # first name
+    [\s|-]    # separator (could be space or hyphen)
+    [a-zA-Z]  # middle name
+    [\s|-]    # separator
+    [a-zA-Z]$ # last name
+    ''', re.VERBOSE)
+
     # ask user for string to search
     print("Enter full name in this format - first middle last")
     name_source = str(input("$ "))
-    # TODO: regex matching and other stuff
+    name_search_object = name_regex.search(name_source)
 
 
 if __name__ == "__main__":

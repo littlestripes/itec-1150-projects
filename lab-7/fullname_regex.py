949cfd0642b915c881888c76437761c0f0f004f3
thevalstar <94736197+thevalstar@users.noreply.github.com>
2022-11-04 15:50:57 -0500

    First commit

---
+++ lab-7/fullname_regex.py
@@ -0,0 +1,21 @@
+"""
+Program Name: fullname_regex.py
+Author: Val Kamali
+Email: val.kamali@go.minnstate.edu
+Description:
+    Determines whether or not input text is a full name.
+Dates: 2022-11-04 - File created.
+"""
+
+import re  # compile(), search()
+
+
+def main():
+    # ask user for string to search
+    print("Enter full name in this format - first middle last")
+    name_source = str(input("$ "))
+    # TODO: regex matching and other stuff
+
+
+if __name__ == "__main__":
+    main()
